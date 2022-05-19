2d0a6081a0a8425821c4e80339df2a78518c4d8d
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-19 02:17:25 +0200

    creazione pagine vue di main, prefooter e footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
     <AppHeader/>
+    <AppMain/>
+    <AppPreFooter/>
+    <AppFooter/>
+    
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppPreFooter from './components/AppPreFooter.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppMain,
+    AppPreFooter,
+    AppFooter,
 }
 }
 </script>
 
 <style lang="scss">
 @import "./style/common.scss"
+
+
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="pre-footer">
+      <div class="container">
+          
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
